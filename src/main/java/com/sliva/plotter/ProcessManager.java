5f7f2cb745f34d62a3eb955113ccd4a0332c126e
Sergei Goshko <serge@keeperlink.com>
2021-06-03 00:29:15 -0400

    pick for move volume with the largest free space

--- src/main/java/com/sliva/plotter/ProcessManager.java
+++ src/main/java/com/sliva/plotter/ProcessManager.java
@@ -30,7 +30,7 @@ import java.util.stream.Stream;
  */
 public class ProcessManager {
 
-    private static final String VERSION = "1.0.7";
+    private static final String VERSION = "1.0.8";
     private static final long MIN_SPACE = 109_000_000_000L;
     private static final File STOP_FILE = new File("plotting-stop");
     private static final File PLOTTING_LOG_FILE = new File("plotting.log");
@@ -197,14 +197,14 @@ public class ProcessManager {
 
     /**
      * Get list of available destinations. List first destination that are not
-     * used by direct plotting and ones with the lowest fill ratio.
+     * used by direct plotting and ones with the largest free space.
      *
      * @return Collection of destination File objects
      */
     private Collection<File> getMoveDestinationsList() {
         synchronized (inUseDirectDest) {
             return getAvailableDestinations().stream()
-                    .sorted(Comparator.comparing((File f) -> inUseDirectDest.contains(f)).thenComparingDouble(this::getFillRatio))
+                    .sorted(Comparator.comparing((File f) -> inUseDirectDest.contains(f)).thenComparing(Comparator.comparingDouble(this::getFreeSpace).reversed()))
                     .collect(Collectors.toList());
         }
     }
